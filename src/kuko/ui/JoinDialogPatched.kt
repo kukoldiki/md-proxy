d3dc4309e74c0ecdae04ab5bddc46e3980808b21
kuko <262641371+kukoldiki@users.noreply.github.com>
2026-07-28 20:48:02 +0500

    fix usids

--- src/kuko/ui/JoinDialogPatched.kt
+++ src/kuko/ui/JoinDialogPatched.kt
@@ -1,9 +1,11 @@
 package kuko.ui
 
 import arc.Core
+import arc.math.Rand
 import arc.scene.ui.Dialog
 import arc.util.Reflect
 import arc.util.Time
+import arc.util.serialization.Base64Coder
 import kuko.net.ProxyListener
 import mindustry.Vars
 import mindustry.ui.dialogs.JoinDialog
@@ -37,6 +39,10 @@ class JoinDialogPatched : JoinDialog() {
             Reflect.set(JoinDialog::class.java, this, "lastPort", port)
 
             val localPort = ProxyListener.listen(ip, port)
+            Core.settings.put(
+                "usid-$ip:$port",
+                getUsid("$ip:$port")
+            )
 
             Vars.net.connect("localhost", localPort) {
                 if (Vars.net.client()) {
@@ -45,5 +51,23 @@ class JoinDialogPatched : JoinDialog() {
                 }
             }
         }
+    }
+}
+
+fun getUsid(ip: String): String? {
+    //consistently use the latter part of an IP, if possible
+    var ip = ip
+    if (ip.contains("/")) {
+        ip = ip.substring(ip.indexOf("/") + 1)
+    }
+
+    if (Core.settings.getString("usid-$ip", null) != null) {
+        return Core.settings.getString("usid-$ip", null)
+    } else {
+        val bytes = ByteArray(8)
+        Rand().nextBytes(bytes)
+        val result = String(Base64Coder.encode(bytes))
+        Core.settings.put("usid-$ip", result)
+        return result
     }
 }
